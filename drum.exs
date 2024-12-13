338f9a1c16dd200da27a7978de8b2c31b44fc819
octetta <joseph.stewart@gmail.com>
2024-12-13 09:38:21 -0800

    Make this a bit more lively...

--- drum.exs
+++ drum.exs
@@ -25,7 +25,16 @@ defmodule Drum do
     wire("v33l10", socket)
   end
 
-  def drum(tempo, socket) do
+  def bass(0, socket) do
+    wire("v0l0v0l0", socket)
+  end
+
+  def bass(pos, socket) do
+    wire("v0l5f220v1l.1", socket)
+  end
+
+  def drum(tempo, pos, socket) do
+    bass(rem(pos,2), socket)
     kick(socket)
     beat(tempo)
     hh(socket)
@@ -33,18 +42,35 @@ defmodule Drum do
     snare(socket)
     beat(tempo)
     hh(socket)
-    beat(tempo)
-    drum(tempo, socket)
+    beat(div(tempo,3))
+    hh(socket)
+    beat(div(tempo,3))
+    hh(socket)
+    beat(div(tempo,3))
+    drum(tempo, pos+1, socket)
   end
   
-  def loop(tempo) do
+  def loop(tempo, pos) do
     {:ok, socket} = Drum.init
-    wire("<p13 <p5 <p11 v33w7p13 v32w7p5 v34w7p11", socket)
-    drum(tempo, socket)
+    wire("<p0 <p1 <p2 <p3 <p4 <p5", socket)
+    wire("<p6 <p7 <p8 <p9 <p10 <p11", socket)
+    wire("<p12 <p13 <p14 <p15 <p16 <p17", socket)
+    wire("<p18 <p19 <p20 <p21 <p22 <p23", socket)
+    wire("<p24", socket)
+    wire("v1w0f1", socket)
+    wire("v0w0f110F1", socket)
+    wire("v33w7p13", socket)
+    wire("v32w7p5", socket)
+    wire("v34w7p11", socket)
+    drum(tempo, pos, socket)
+  end
+
+  def loop(tempo) do
+    loop(tempo,0)
   end
 
   def loop do
-    loop(150)
+    loop(150,0)
   end
 end
 
